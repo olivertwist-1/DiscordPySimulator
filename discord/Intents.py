ffae3910e8934c4da9540ab98398d49db7ea8507
Juan Sebastián <89955811+juansebastian2006@users.noreply.github.com>
2022-01-18 07:01:07 -0300

    Create Intents.py

---
+++ discord/Intents.py
@@ -0,0 +1,9 @@
+class Intents:
+
+    @classmethod
+    def all(cls) -> int:
+        return 1 # maybe will add something here later
+
+    @classmethod
+    def default(cls) -> int:
+        return 0 # same 
